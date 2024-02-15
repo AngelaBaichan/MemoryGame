033dc872c7bd71a0676caf8dfca75f4718b70978
Angela Baichan <angelaramnanan03032007@gmail.com>
2024-02-15 13:15:50 -0500

    Initial Commit

---
+++ RacingGame.py
@@ -0,0 +1,18 @@
+# Author
+# Data
+# Description : Memory Game with turtle
+# Revision History
+# Name     Date      Desc
+# scl      2/12/24      Intitial code base
+#
+#
+
+import time
+import turtle
+import random as rd
+
+start_x = -200
+start_y = 150
+
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
