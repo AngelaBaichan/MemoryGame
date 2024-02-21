bd0390f7e0532bd319eb62cecc30744720736ba7
Angela Baichan <angelaramnanan03032007@gmail.com>
2024-02-21 14:44:44 -0500

    Initial Commit

---
+++ Angela Do now file.py
@@ -0,0 +1,16 @@
+# excerise 2
+
+def main():
+    x = 5
+    if x >= 100:
+        print("x = ", x)
+        print("LARGE NUMBER!")
+
+    x = 200
+    if x >= 100:
+        print("x= " ,x)
+        print("LARGE NUMBER!")
+        
+
+
+main()
