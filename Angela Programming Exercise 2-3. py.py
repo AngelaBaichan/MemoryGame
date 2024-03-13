7ea47a720aed9b6d1882901c81f3908439ea1144
Angela Baichan <angelaramnanan03032007@gmail.com>
2024-03-13 12:35:52 -0400

    Message

---
+++ Angela Programming Exercise 2-3. py.py
@@ -0,0 +1,11 @@
+
+tractSize = 0.0
+acres = 0.0
+
+SQ_FEET_PER_ACRE = 43560
+
+tractSize = float(input("enter the number of square feet in the tract"))
+
+acres = tractSize * float(input("Enter a umber"))
+
+print("The size of that tract is", format(acres,'2f'),"acres.")
---
+++ Angela Programming Exercise 2-5.py.py
@@ -0,0 +1,15 @@
+distance6Hours = 0.0
+distance10Hours = 0.0
+distance15Hours = 0.0
+
+SPEED = 70
+
+distance6Hours = SPEED * 6
+distance10Hours = SPEED * 10
+distance15Hours = SPEED * 15
+
+
+print ("The call will travel the following distances:")
+print (distance6Hours,"miles in 6 hours.")
+print (distance10Hours,"miles in 10 hourse.")
+print (distance15Hours,"miles in 15 hours.")
